83da4c0fd4334fac6574dbc211270d4c599790ea
Kerim <MCKerim@gmx.de>
2022-08-15 15:59:23 +0200

    ConstantNumberOfEnemysStage Added
    - Selects random number of enemys from AmountOfConstantEnemysChances
    - Spawns this amount of enemys
    - Spawns instant new enemy for every destroyed enemy
    - Is over if randomly selected number of enemys are spawned

---
+++ Tap Tap/Assets/Scripts/Spawning/Stages/ConstantNumberOfEnemysStage.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantNumberOfEnemysStage : MonoBehaviour, IStage
+{
+    [SerializeField] private EnemyEntry[] enemys;
+    private EnemySpawnFunctions enemySpawnFunctions;
+
+    [SerializeField] private bool isEndless;
+    private int numberOfEnemysTillOver;
+    [SerializeField] private int minNumberOfEnemysTillOver;
+    [SerializeField] private int maxNumberOfEnemysTillOver;
+
+    private int amountOfConstantEnemys;
+    [SerializeField] private AmountChances[] amountOfConstantEnemysChances;
+    private bool isOver;
+
+    private void Start()
+    {
+        enemySpawnFunctions = GameObject.FindObjectOfType<EnemySpawnFunctions>();
+    }
+
+    public void Reset()
+    {
+        if (!isEndless)
+        {
+            numberOfEnemysTillOver = Random.Range(minNumberOfEnemysTillOver, maxNumberOfEnemysTillOver + 1);
+        }
+        amountOfConstantEnemys = enemySpawnFunctions.SelectRandomAmount(amountOfConstantEnemysChances);
+        isOver = false;
+
+        Debug.Log("Amount Constant enemys: " + amountOfConstantEnemys);
+        Debug.Log("numberOfEnemysTillOver: " + numberOfEnemysTillOver);
+    }
+
+    public bool IsOver()
+    {
+        return isOver;
+    }
+
+    public void UpdateStage()
+    {
+        if (IsOver())
+        {
+            return;
+        }
+
+        int currentEnemys = GameObject.FindObjectsOfType<Enemy>().Length;
+
+        if (!isEndless)
+        {
+            if (numberOfEnemysTillOver <= 0 && currentEnemys == 0)
+            {
+                isOver = true;
+                return;
+            }
+        }
+        
+        for(int i = currentEnemys; i < amountOfConstantEnemys && (numberOfEnemysTillOver > 0 || isEndless); i++)
+        {
+            enemySpawnFunctions.SpawnRandomEnemy(enemys);
+            if (!isEndless)
+            {
+                numberOfEnemysTillOver--;
+            }
+        }
+    }
+}
